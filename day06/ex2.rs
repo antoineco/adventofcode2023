b8d9f7814340a01eeaac6ac6859a29ad80d488d3
Antoine Cotten <hello@acotten.com>
2024-06-25 15:03:13 +0200

    Day 06 (binary search)
    Obvious optimization, reduces the run time from 2-3ms to 20-30µs on the
    given input.

--- day06/ex2.rs
+++ day06/ex2.rs
@@ -2,7 +2,6 @@ use core::panic;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
-use std::ops::ControlFlow;
 use std::time::Instant;
 
 fn main() -> io::Result<()> {
@@ -43,17 +42,26 @@ fn ways<R: io::Read>(r: R) -> u64 {
      *     .sum()
      */
 
-    if let ControlFlow::Break(first_win) = (1..race_time).try_for_each(|press_time| {
-        if is_win(press_time, race_time, race_dist) {
-            ControlFlow::Break(press_time)
-        } else {
-            ControlFlow::Continue(())
+    let mut i = 0;
+    let mut j = race_time / 2;
+    let first_win = loop {
+        if i > j {
+            break 0;
         }
-    }) {
-        (race_time + 1) - first_win * 2
-    } else {
-        0
-    }
+        let mid = i + (j - i) / 2;
+        if is_win(mid, race_time, race_dist) {
+            if !is_win(mid - 1, race_time, race_dist) {
+                break mid;
+            }
+            j = mid;
+        } else {
+            if is_win(mid + 1, race_time, race_dist) {
+                break mid + 1;
+            }
+            i = mid;
+        }
+    };
+    (race_time + 1) - first_win * 2
 }
 
 fn is_win(press_time: u64, race_time: u64, race_dist: u64) -> bool {
